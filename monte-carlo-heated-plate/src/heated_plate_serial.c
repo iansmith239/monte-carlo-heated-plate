fedbfa8b8dc1b12fa30a21f04250fae7fe7b9772
Ian <iansmith239@gmail.com>
2016-07-09 12:42:56 -0700

    Added main algo

--- monte-carlo-heated-plate/src/heated_plate_serial.c
+++ monte-carlo-heated-plate/src/heated_plate_serial.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : heated_plate_serial.c
- Author      : 
+ Author      :
  Version     :
  Copyright   :
  Description : Serial Version of Heated Plate
@@ -13,7 +13,16 @@
 
 typedef enum { false, true } bool;
 
+typedef struct {
+    int x;
+    int y;
+} point;
+
 int main (int argc, char *argv[]) {
+
+	int plate_x = 500;
+	int plate_y = 500;
+	int num_edge_searches = 100;
 
 	//1. Get 4 temps from user
 	int temp_top = atoi(argv[1]);
@@ -24,7 +33,7 @@ int main (int argc, char *argv[]) {
 	printf("Temps are [T=%d,L=%d,R=%d,B=%d]",temp_top,temp_left,temp_right,temp_bottom);
 
 	//2. Create and initialize the array
-	double temps[500][500];
+	double temps[plate_x][plate_y];
 
 	//Initialize top
 
@@ -35,11 +44,35 @@ int main (int argc, char *argv[]) {
 	//Initialize right
 
 	//3. Iterate through the array in sequence starting from the bottom corner
+	for (int x = 0; x < plate_x; x++) {
+		for (int y = 0; y < plate_y; y++) {
+			//Get info for point
+			point p = {x,y};
 
-	//4. Move in random direction
+			//Search for the edge many times
+			double p_edge_temp_sum = 0d;
+			int i = 0;
+			for (i = 0; i < num_edge_searches; i++) {
+				//Get the next point
+				point pn = get_rndm_ngbr(p);
+				while (!is_edge(pn)) {
+					pn = get_rndm_ngbr(pn);
+				}
+				p_edge_temp_sum += temps[pn.x,pn.y]
+			}
+
+			temps[x][y] = p_edge_temp_sum / (i+1);
+		}
+	}
 }
 
-bool is_edge(int x, int y) {
+point get_rndm_ngbr(point p) {
+	//randomly select a number 1 to 4
+	//1=T,2=L,3=R,4=B
+
+}
+
+bool is_edge(point p) {
 	return false;
 }
 
